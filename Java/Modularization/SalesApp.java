081428caaae96d35c785fd307008ddedf00d0e8f
Wanga-dgaf <pm0658241889@gmail.com>
2026-08-05 10:50:53 +0200

    Refactor SalesApp to improve calculation methods and display output

--- Java/Modularization/SalesApp.java
+++ Java/Modularization/SalesApp.java
@@ -2,9 +2,33 @@ import java.util.Scanner;
 
 public class SalesApp {
 
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the price");
+		double price = sc.nextDouble();
+		
+		System.out.println("Enter number of items");
+		int noOfItems = sc.nextInt();
+		
+		//double total = price * noOfItems;
+		double cost = getTotal(price, noOfItems);
+		double discount = deterDiscount(cost);
+		double vat = getVat(cost);
+		double finalAmt = getTotalAmt(cost);
+		
+		
+		displayInfo(cost, discount, vat, finalAmt);
+		
+		
+	}
+
 	//Access modifier....... return type.....methodName....parameters
-	public static void displayInfo(double total){
+	public static void displayInfo(double total, double deterDiscount, double getVat, double getTotalAmt){
 		System.out.println("Total : (VAT exclusive)" + total);
+		System.out.println("discount: " + deterDiscount(total));
+		System.out.println("VAT Amount: " + getVat(total));
+		System.out.println("Total Amount: " + getTotalAmt(total));
 	}
 	
 	public static double getTotal(double price, int noOfItems){
@@ -35,29 +59,7 @@ public class SalesApp {
 	//function called getTotalAmt that receives total amt and %
 	
 	public static double getTotalAmt(double total){
-		return total - deterDiscount(total) - getVat(total);
+		return total - deterDiscount(total) + getVat(total);
 	}
 	
-	public static void main(String [] args){
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter the price");
-		double price = sc.nextDouble();
-		
-		System.out.println("Enter number of items");
-		int noOfItems = sc.nextInt();
-		
-		//double total = price * noOfItems;
-		double total = getTotal(price, noOfItems);
-		
-		
-		displayInfo(total);
-		System.out.println("discount: " + deterDiscount(total));
-		System.out.println("VAT Amount: " + getVat(total));
-		System.out.println("Total Amount: " + getTotalAmt(total));
-		
-	}
-	
-	
-
 }
