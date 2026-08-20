19ac245a31c90d74bbcb878105b7826ae0ad48ec
Wanga-dgaf <pm0658241889@gmail.com>
2026-08-20 21:16:34 +0200

    Add ClassTest3_042006.java with initial structure and isValidFormat method

---
+++ Java/String Manipulation/ClassTest3_042006.java
@@ -0,0 +1,11 @@
+public class ClassTest3_042006 {
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean isValidFormat(String userLogin){
+        if (userLogin.indexOf("#")){
+
+        }
+    }
+}
