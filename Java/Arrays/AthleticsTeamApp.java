abb5c4d3c16911b483deb80db29e2df352e85b22
Wanga-dgaf <pm0658241889@gmail.com>
2026-09-15 21:12:23 +0200

    Initialize distanceRun array in AthleticsTeamApp

--- Java/Arrays/AthleticsTeamApp.java
+++ Java/Arrays/AthleticsTeamApp.java
@@ -2,7 +2,7 @@ public class AthleticsTeamApp {
     public static void main(String[] args){
         String[] membersNames = {"Sello M", "Jason P", "Lethu A", "Banda B", "Johnn B", "Wicky J"};
         int[] membersAges = {35, 42, 42, 25, 32, 49};
+        double[] distanceRun = new double[5];
 
-        
     }
 }
