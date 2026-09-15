65f1dea576a63f56d8fee0e7ce7cd9eefe10a315
Wanga-dgaf <pm0658241889@gmail.com>
2026-09-16 00:26:46 +0200

    Enhance AthleticsTeamApp to capture and display distances run by team members, including average distance calculation and identifying the fastest runner.

--- Java/Arrays/AthleticsTeamApp.java
+++ Java/Arrays/AthleticsTeamApp.java
@@ -1,8 +1,73 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class AthleticsTeamApp {
     public static void main(String[] args){
-        String[] membersNames = {"Sello M", "Jason P", "Lethu A", "Banda B", "Johnn B", "Wicky J"};
-        int[] membersAges = {35, 42, 42, 25, 32, 49};
-        double[] distanceRun = new double[5];
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("##.0");
 
+
+        String[] membersNames = {"Sello M.", "Jason P.", "Lethu A.", "Banda B.", "Johnn B.", "Wicky J."};
+        int[] membersAges = {35, 42, 42, 25, 32, 49};
+        double[] distanceRun = new double[6];
+        //Variables
+        int count = 0;
+        double km = 0;
+
+
+        for (int i = 0; i < membersNames.length; i++){
+            System.out.print("Enter distance ran by " + membersNames[i] + " (in kilometers): "); 
+            km = sc.nextDouble();
+            distanceRun[i] = km;
+        }
+
+        double average = calcAvgDistance(distanceRun);
+        double fastetstRunner = determineFastRunner(distanceRun, average);
+        
+        displayMemberDetails(membersNames, membersAges, distanceRun, fastetstRunner, count);
+        
+
+    }
+
+    public static double calcAvgDistance (double[] distanceRun){
+        double count = 0, totalDistance = 0, average = 0;
+
+        for (int i = 0; i < distanceRun.length; i++){
+            
+            totalDistance += distanceRun[i];
+            count++;
+            
+        }
+        average = totalDistance/count;
+        return average;
+    }
+
+    public static int determineFastRunner(double[] distanceRun, double avgDistances){
+        int count = 0;
+        avgDistances = calcAvgDistance(distanceRun);
+        for (int i = 0; i < distanceRun.length; i++){
+            
+            if (avgDistances < distanceRun[i]){} count++;
+            
+        }
+        return count;
+    }
+
+    public static void displayMemberDetails(String[] membersNames, int[] membersAges, double[] distanceRun, double avgDistance, int count){
+    
+        count = determineFastRunner(distanceRun, avgDistance);
+        avgDistance = calcAvgDistance(distanceRun);
+        System.out.println("Club members details: ");
+        System.out.printf("%-15s %-10s %-10s%n","Name", "Age", "Distance");
+        for (int i = 0; i < membersNames.length; i++){
+            
+            System.out.printf("%-15s %-10d %-10.1f%n",membersNames[i], membersAges[i], distanceRun[i]);
+            
+        }
+
+        System.out.println(membersNames[count] + " run is above the average distance by club members ");
+        System.out.printf("Average distance Run by Club Members: %.1f", avgDistance,  " kilometers");
+        
+        
     }
 }
